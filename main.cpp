72c7f4d3530fdb76a92fa58898b92c3cc6c5240b
tendmdi <Vasechkina05@gmail.com>
2026-05-05 13:46:08 +0500

    поменяла местами,мэйн поставила в самый конец потому что вылазит ошибка,и как я поняла компилятор просто не видит мои типы когда читает мэин

--- main.cpp
+++ main.cpp
@@ -5,6 +5,51 @@ using namespace std;
 
 const double PI = 3.141592653589793;
 
+
+struct point3d
+{
+    double x, y, z;
+
+    point3d(double x=0.0, double y=0.0, double z=0.0)
+        : x(x), y(y), z(z) {}
+
+    void print() const
+    {
+        cout << "x=" << x << " y=" << y << " z=" << z << endl;
+    }
+    double getBackX() const { return x; }
+    double getBackY() const { return y; }
+    double getBackZ() const { return z; }
+};
+#include <cmath>
+#include <random>
+
+class TorusGenerator
+{
+private:
+    double R, r;
+    std::mt19937 gen;
+    std::uniform_real_distribution<> dist;
+
+public:
+    TorusGenerator(double R, double r)
+        : R(R), r(r), dist(0.0, 2*PI)
+    {
+        gen.seed(std::random_device{}());
+    }
+
+    point3d rnd()
+    {
+        double u = dist(gen);
+        double v = dist(gen);
+
+        double x = (R + r*cos(v)) * cos(u);
+        double y = (R + r*cos(v)) * sin(u);
+        double z = r * sin(v);
+
+        return point3d(x, y, z);
+    }
+};
 int main()
 {
     int K;
@@ -74,47 +119,3 @@ int main()
 
     return 0;
 }
-struct point3d
-{
-    double x, y, z;
-
-    point3d(double x=0.0, double y=0.0, double z=0.0)
-        : x(x), y(y), z(z) {}
-
-    void print() const
-    {
-        cout << "x=" << x << " y=" << y << " z=" << z << endl;
-    }
-    double getBackX() const { return x; }
-    double getBackY() const { return y; }
-    double getBackZ() const { return z; }
-};
-#include <cmath>
-#include <random>
-
-class TorusGenerator
-{
-private:
-    double R, r;
-    std::mt19937 gen;
-    std::uniform_real_distribution<> dist;
-
-public:
-    TorusGenerator(double R, double r)
-        : R(R), r(r), dist(0.0, 2*PI)
-    {
-        gen.seed(std::random_device{}());
-    }
-
-    point3d rnd()
-    {
-        double u = dist(gen);
-        double v = dist(gen);
-
-        double x = (R + r*cos(v)) * cos(u);
-        double y = (R + r*cos(v)) * sin(u);
-        double z = r * sin(v);
-
-        return point3d(x, y, z);
-    }
-};
