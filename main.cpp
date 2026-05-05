a2d51d5eaeb52704369d60e8e18c64e996c2bb3f
tendmdi <Vasechkina05@gmail.com>
2026-05-05 13:08:43 +0500

    Добавлен генератор случайных точек в торе,нашла в интернете формулы точек,пока что все горит крсным но дай бог все заработает,тор это своеобразный бублик R-больной радиус ,r-маленький радиус , еще добаалена пи в начало файла

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+const double PI = 3.141592653589793;
+
 int main()
 {
     return 0;
@@ -15,5 +17,34 @@ struct point3d
     void print() const
     {
         cout << "x=" << x << " y=" << y << " z=" << z << endl;
+    }
+};
+#include <cmath>
+#include <random>
+
+class TorusGenerator
+{
+private:
+    double R, r;
+    std::mt19937 gen;
+    std::uniform_real_distribution<> dist;
+
+public:
+    TorusGenerator(double R, double r)
+        : R(R), r(r), dist(0.0, 2*PI)
+    {
+        gen.seed(std::random_device{}());
+    }
+
+    point3d rnd()
+    {
+        double u = dist(gen);
+        double v = dist(gen);
+
+        double x = (R + r*cos(v)) * cos(u);
+        double y = (R + r*cos(v)) * sin(u);
+        double z = r * sin(v);
+
+        return point3d(x, y, z);
     }
 };
