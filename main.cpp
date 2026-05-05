889f235dd652edb22dc464202f5d8124c39ea90a
tendmdi <Vasechkina05@gmail.com>
2026-05-05 12:55:54 +0500

    Добавлена структура point3d

--- main.cpp
+++ main.cpp
@@ -5,3 +5,15 @@ int main()
 {
     return 0;
 }
+struct point3d
+{
+    double x, y, z;
+
+    point3d(double x=0.0, double y=0.0, double z=0.0)
+        : x(x), y(y), z(z) {}
+
+    void print() const
+    {
+        cout << "x=" << x << " y=" << y << " z=" << z << endl;
+    }
+};
