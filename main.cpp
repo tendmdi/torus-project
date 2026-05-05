dae0b41bc3916ccefb0975ce8c24b2dc41dbacd7
tendmdi <Vasechkina05@gmail.com>
2026-05-05 13:37:22 +0500

    Добавлено дабл getBack в стракт поинn3d и добавлено меню и сохранение в файл

--- main.cpp
+++ main.cpp
@@ -16,6 +16,62 @@ int main()
     for(int i = 0; i < K; i++)
         points.push_back(gen.rnd());
 
+    int choice;
+
+    do {
+        cout << "\n1. Pokazat tochky\n";
+        cout << "2. Pokazat koordinaty\n";
+        cout << "3. Dobavit tochky\n";
+        cout << "4. Sohranit v fail\n";
+        cout << "0. Vixod\n";
+
+        cin >> choice;
+
+        if(choice == 1)
+        {
+            int i;
+            cout << "Vvedite index: ";
+            cin >> i;
+            if(i >= 0 && i < points.size())
+                points[i].print();
+        }
+
+        if(choice == 2)
+        {
+            int i;
+            char c;
+            cout << "Vvedite index i koordinaty (x/y/z): ";
+            cin >> i >> c;
+
+            if(i >= 0 && i < points.size())
+            {
+                if(c=='x') cout << points[i].getBackX() << endl;
+                if(c=='y') cout << points[i].getBackY() << endl;
+                if(c=='z') cout << points[i].getBackZ() << endl;
+            }
+        }
+
+        if(choice == 3)
+        {
+            double x,y,z;
+            cout << "vvedite x y z: ";
+            cin >> x >> y >> z;
+            points.push_back(point3d(x,y,z));
+        }
+
+        if(choice == 4)
+        {
+            ofstream file("points.txt");
+
+            for(auto &p: points)
+                file << p.x << " " << p.y << " " << p.z << endl;
+
+            file.close();
+            cout << "Soxraneno v points.txt\n";
+        }
+
+    } while(choice != 0);
+
     return 0;
 }
 struct point3d
@@ -29,6 +85,9 @@ struct point3d
     {
         cout << "x=" << x << " y=" << y << " z=" << z << endl;
     }
+    double getBackX() const { return x; }
+    double getBackY() const { return y; }
+    double getBackZ() const { return z; }
 };
 #include <cmath>
 #include <random>
